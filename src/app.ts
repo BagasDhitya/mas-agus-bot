35553e87b1e665e45342bd64b45165a0dc551744
codewaveindonesia04 <code.wave.consulting@gmail.com>
2024-12-11 14:56:18 +0700

    change cors setting

--- src/app.ts
+++ src/app.ts
@@ -8,7 +8,7 @@ const PORT = 3000;
 
 app.use(
   cors({
-    origin: ["http://localhost:5173/"],
+    origin: ["http://localhost:5173"],
     methods: ["GET", "POST"],
     allowedHeaders: ["Content-Type"],
   })
