a505dc76b7bd759a9ca1f564e66c44f0c59c6dc1
codewaveindonesia04 <code.wave.consulting@gmail.com>
2024-12-11 14:35:05 +0700

    config cors

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,16 @@
 import express, { Application } from "express";
-import cookieParser from 'cookie-parser';
+import cookieParser from "cookie-parser";
 import ChatRouter from "./routers/chat.router";
+import cors from "cors";
 
 const app: Application = express();
 const PORT = 3000;
 
+app.use(
+  cors({
+    origin: "https://mas-agus-bot.vercel.app",
+  })
+);
 app.use(express.json());
 app.use(cookieParser());
 
